05a382825262740c24cc7db071328ffcb3a7f4bc
Meljn <ilya.mel2018@mail.ru>
2026-05-26 19:33:36 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Course_project_main/',
+  base: '/Cource_project_main/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
